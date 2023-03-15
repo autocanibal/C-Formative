a4a68bbd6c25cfb27eb1f94d70ae2489ce5b995f
monde <mondzzmkhize@gmail.com>
2023-03-15 13:45:36 +0200

    Added a variable

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int randomNum = rnd.Next();
+        Console.WriteLine(randomNum);
+        Thread.Sleep(1000);
+        for(int i = 0; ; i++) { Console.WriteLine(i); if (i == randomNum) break; }
+    }
+}

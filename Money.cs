80249842aaaf01d0ebe92b6841464b1fdb52ae74
Monde <mondzzmkhize@gmail.com>
2023-03-20 20:04:50 +0200

    Added new file for money

---
+++ Money.cs
@@ -0,0 +1,9 @@
+﻿namespace C_Formative
+{
+    internal class Money
+    {
+        private double grossIncome, monthlyDeductions, monthlyExpense, rentalAmount, purchasePrice, interestRate, deposit;
+        private int repaymentMonths;
+        private bool rent, buy;
+    }
+}

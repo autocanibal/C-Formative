8e6559fca3ed1427cbc1cec7f591c62a3ff34cdf
monde <mondzzmkhize@gmail.com>
2023-03-21 17:48:57 +0200

    finished adding setters

--- Money.cs
+++ Money.cs
@@ -4,18 +4,47 @@
     {
         private double grossIncome, monthlyDeductions, monthlyExpense, rentalAmount, purchasePrice, interestRate, deposit;
 
-        public void setMonthlyDeductions(double monthlyDeductions)
-        {
-            this.monthlyDeductions = monthlyDeductions;
-        }
-
         public void setGrossIncome(double grossIncome)
         {
             this.grossIncome = grossIncome;
         }
 
+        public void setMonthlyDeductions(double monthlyDeductions)
+        {
+            this.monthlyDeductions = monthlyDeductions;
+        }
+        
+        public void setRentalAmount(double rentalAmount)
+        {
+            this.rentalAmount = rentalAmount;
+        }
+        
+        public void setPurchasePrice(double purchasePrice)
+        {
+            this.purchasePrice = purchasePrice;
+        }
+        
+        public void setInterestRate(double interestRate)
+        {
+            this.interestRate = interestRate;
+        }
+        
+        public void setDeposit(double deposit)
+        {
+            this.deposit = deposit;
+        }
+
         private int repaymentMonths;
         private bool rent, buy;
+        
+        public void setRepaymentMonths(int repaymentMonths)
+        {
+            this.repaymentMonths = repaymentMonths;
+        }
+        public void setPurchasePrice(double purchasePrice)
+        {
+            this.purchasePrice = purchasePrice;
+        }
 
         
     }

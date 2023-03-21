3c49f3b37b659e0775fd370a8697730d1dc3d020
monde <mondzzmkhize@gmail.com>
2023-03-21 13:59:28 +0200

    Added setters

--- Money.cs
+++ Money.cs
@@ -3,7 +3,20 @@
     internal class Money
     {
         private double grossIncome, monthlyDeductions, monthlyExpense, rentalAmount, purchasePrice, interestRate, deposit;
+
+        public void setMonthlyDeductions(double monthlyDeductions)
+        {
+            this.monthlyDeductions = monthlyDeductions;
+        }
+
+        public void setGrossIncome(double grossIncome)
+        {
+            this.grossIncome = grossIncome;
+        }
+
         private int repaymentMonths;
         private bool rent, buy;
+
+        
     }
 }

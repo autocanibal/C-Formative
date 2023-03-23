04e49c19a895b55c655e8c252d3482e4b46c636e
Monde <mondzzmkhize@gmail.com>
2023-03-23 17:39:59 +0200

    Added rental functionality

--- Money.cs
+++ Money.cs
@@ -3,49 +3,28 @@
     internal class Money
     {
         private double grossIncome, monthlyDeductions, monthlyExpense, rentalAmount, purchasePrice, interestRate, deposit;
+        private double incomeAfterTax, remainingMoney;
 
-        public void setGrossIncome(double grossIncome)
-        {
-            this.grossIncome = grossIncome;
-        }
 
-        public void setMonthlyDeductions(double monthlyDeductions)
-        {
-            this.monthlyDeductions = monthlyDeductions;
-        }
 
-        public void setMonthlyExpense(double monthlyExpense)
-        {
-            this.monthlyExpense = monthlyExpense;
-        }
+        public void setGrossIncome(double grossIncome){ this.grossIncome = grossIncome; }
 
-        public void setRentalAmount(double rentalAmount)
-        {
-            this.rentalAmount = rentalAmount;
-        }
+        public void setMonthlyDeductions(double monthlyDeductions){ this.monthlyDeductions = monthlyDeductions; }
 
-        public void setPurchasePrice(double purchasePrice)
-        {
-            this.purchasePrice = purchasePrice;
-        }
+        public double setIncomeAfterTax(double grossIncome, double monthlyDeductions) { incomeAfterTax = grossIncome - monthlyDeductions; return incomeAfterTax; }
+        
+        public void setMonthlyExpenses(double monthlyExpense){ this.monthlyExpense = monthlyExpense; }
+        
+        public void setRemainingMoney(double incomeAfterTax, double monthlyExpense) { remainingMoney = incomeAfterTax - monthlyExpense; }
 
-        public void setInterestRate(double interestRate)
-        {
-            this.interestRate = interestRate;
-        }
+        public void setInterestRate(double interestRate){ this.interestRate = interestRate; }
 
-        public void setDeposit(double deposit)
-        {
-            this.deposit = deposit;
-        }
+        public void setDeposit(double deposit){ this.deposit = deposit; }
 
         private int repaymentMonths;
         private bool rent, buy;
 
-        public void setRepaymentMonths(int repaymentMonths)
-        {
-            this.repaymentMonths = repaymentMonths;
-        }
+        public void setRepaymentMonths(int repaymentMonths){ this.repaymentMonths = repaymentMonths; }
 
         public void setType(int type)
         {
@@ -55,8 +34,9 @@
                 rent = false;
                 rentalAmount = 0;
                 Console.WriteLine("Please enter the purchase amount: ");
-                Console.WriteLine(buy);
-                Console.WriteLine(rent);
+                purchasePrice = double.Parse(Console.ReadLine());
+
+                
             }
             else if (type == 0)
             {
@@ -64,9 +44,14 @@
                 buy = false;
                 purchasePrice = 0;
                 Console.WriteLine("Please enter your rental amount: ");
-                Console.WriteLine(buy);
-                Console.WriteLine(rent);
+                rentalAmount = double.Parse(Console.ReadLine());
+                double availableFunds = remainingMoney - rentalAmount;
+                Console.WriteLine("Your available funds after rent is: R"+availableFunds);
+
+                
             }
         }
+
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,19 +6,42 @@ class Program
     static void Main(string[] args)
     {
         Money current = new Money();
+
         Console.WriteLine("Welcome!!");
         Console.WriteLine("This application will help you, whether buying or renting.");
         Console.WriteLine("");
-        Console.WriteLine("Firstly, please enter your monthly gross income before deductions: ");
-        double grossIncome = double.Parse(Console.ReadLine());
-        Console.WriteLine("\nSecondly, please enter your monthly tax deductions: ");
-        double monthlyDeductions = double.Parse(Console.ReadLine());
-        Console.WriteLine("\nThirdly, please enter your monthly living expenses: ");
-        double monthlyExpense = double.Parse(Console.ReadLine());
-        Console.WriteLine("\nNow please choose whether you are renting or buying (0 for renting, 1 for buying)");
-        int type = int.Parse(Console.ReadLine());
+        while (true)
+        {
+            try
+        {
+                Console.WriteLine("Firstly, please enter your monthly gross income before deductions: ");
+                double grossIncome = double.Parse(Console.ReadLine());
+                current.setGrossIncome(grossIncome);
+
+
+                Console.WriteLine("\nSecondly, please enter your monthly tax deductions: ");
+                double monthlyDeductions = double.Parse(Console.ReadLine());
+                current.setMonthlyDeductions(monthlyDeductions);
+                double incomeAfterTax = current.setIncomeAfterTax(grossIncome, monthlyDeductions);
+            
+
+                Console.WriteLine("\nThirdly, please enter your monthly living expenses: ");
+                double monthlyExpenses = double.Parse(Console.ReadLine());
+                current.setMonthlyExpenses(monthlyExpenses);
+                current.setRemainingMoney(incomeAfterTax, monthlyExpenses);
+
+                Console.WriteLine("\nNow please choose whether you are renting or buying (0 for renting, 1 for buying)");
+                int type = int.Parse(Console.ReadLine());
         
-        current.setType(type);
+                current.setType(type);
+                break;
+        }catch (Exception ex)
+        {
+            Console.WriteLine("There was an error. Please start again");
+        }
+        }
+        
+        
 
 
     }
